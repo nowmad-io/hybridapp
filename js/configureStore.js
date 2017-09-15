8bf0c2cf6fa4c026fede5a92bc77da75e642113a
julienr2 <julien.rougeron@nouma.fr>
2017-09-15 17:54:12 +0200

    remove redux-thunk

--- js/configureStore.js
+++ js/configureStore.js
@@ -1,14 +1,13 @@
 import { AsyncStorage } from 'react-native';
 import devTools from 'remote-redux-devtools';
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import reducer from './reducers';
 import promise from './promise';
 
 export default function configureStore(onCompletion:()=>void):any {
   const enhancer = compose(
-    applyMiddleware(thunk, promise),
+    applyMiddleware(promise),
     devTools({
       name: 'traveltnetwork', realtime: true,
     }),
