38a858f145745af77c8f7d991c25cf312ba977b3
julienr2 <julien.rougeron@nouma.fr>
2017-09-23 19:28:38 +0200

    set up redux + react-navigation

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,9 +2,11 @@ import { crudReducer } from 'redux-crud-store';
 
 import auth from './auth';
 import reviews from './reviews';
+import nav from './nav';
 import drawer from './drawer';
 
 export default {
+  nav,
   drawer,
   auth,
   reviews,
