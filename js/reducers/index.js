df215bac74872a091ea813af3ed1a84ddb0881bb
julienr2 <julien.rougeron@nouma.fr>
2017-09-23 19:29:04 +0200

    remove unused reducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -3,11 +3,9 @@ import { crudReducer } from 'redux-crud-store';
 import auth from './auth';
 import reviews from './reviews';
 import nav from './nav';
-import drawer from './drawer';
 
 export default {
   nav,
-  drawer,
   auth,
   reviews,
   models: crudReducer
