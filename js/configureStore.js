51331be7432db606d18ae8ca29bd8a2b9f6b89a5
julienr2 <julien.rougeron@nouma.fr>
2017-11-22 02:19:43 +0100

    keep home reducer

--- js/configureStore.js
+++ js/configureStore.js
@@ -45,7 +45,7 @@ export default function configureStore(onCompletion:()=>void):any {
 
   persistStore(store, {
     storage: AsyncStorage,
-    blacklist: ['nav', 'search', 'home']
+    blacklist: ['nav', 'search']
   }, () => {
     sagaMiddleware.run(requestsSaga(apiConfig()));
 
