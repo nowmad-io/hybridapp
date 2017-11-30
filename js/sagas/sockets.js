2832ad268bfedbc82e6628312266eb77e880d389
julienr2 <julien.rougeron@nouma.fr>
2017-11-30 10:39:13 +0000

    remove all listener on disconnect !

--- js/sagas/sockets.js
+++ js/sagas/sockets.js
@@ -5,8 +5,7 @@ import { RUN_SAGAS, STOP_SAGAS } from '../constants/utils';
 export const _openSocket = (socket) =>
 function * openSocket() {
   const token = yield select((state) => state.auth.token);
-
-  socket.on('connected', function () {
+  socket.on('connected', () => {
     socket.emit('authenticate', { token });
   });
 
@@ -16,6 +15,7 @@ function * openSocket() {
 export const _closeSocket = (socket) =>
 function * closeSocket() {
   socket.close();
+  socket.removeAllListeners();
 }
 
 export default function _root(socket) {
