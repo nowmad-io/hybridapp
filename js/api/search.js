233ba1961dc50f830882b7cda0865fac8a48ee7e
julienr2 <julien.rougeron@nouma.fr>
2017-12-12 20:23:17 +0000

    don't filter by point of interest

--- js/api/search.js
+++ js/api/search.js
@@ -13,8 +13,7 @@ export function getNearbyPlaces(place) {
   const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
         key = `key=${Config.PLACES_API_KEY}`,
         location = `location=${place.latitude},${place.longitude}`,
-        radius = 'radius=10',
-        type = 'type=point_of_interest';
+        radius = 'radius=500';
 
   return apiCall(NEARBY_SUCCESS, NEARBY_ERROR, 'get', `${url}?${key}&${location}&${radius}`);
 }
