624d23ae8fcbc1fc13c6ec44ebdc7f99ce79baec
julienr2 <julien.rougeron@nouma.fr>
2018-02-13 17:11:57 +0700

    remove legacy way of declaring refs

--- js/components/pages/home/index.js
+++ js/components/pages/home/index.js
@@ -32,17 +32,17 @@ class Home extends Component {
 
   componentDidMount() {
     if (this.props.newPlace) {
-      this.refs.searchWrapper.getWrappedInstance().searchCoordinates(this.props.newPlace, true);
+      this._searchWrapper.getWrappedInstance().searchCoordinates(this.props.newPlace, true);
     }
     if (this.props.googlePlace) {
-      this.refs.searchWrapper.getWrappedInstance().setValue(this.props.googlePlace.name);
+      this._searchWrapper.getWrappedInstance().setValue(this.props.googlePlace.name);
     }
   }
 
   componentWillReceiveProps({ fromReview }) {
     if (fromReview) {
-      this.refs.searchWrapper.getWrappedInstance().clear();
-      this.refs.searchWrapper.getWrappedInstance().blurInput();
+      this._searchWrapper.getWrappedInstance().clear();
+      this._searchWrapper.getWrappedInstance().blurInput();
       this.onRegionChangeComplete(this.props.region);
       this.props.dispatch(setFromReview(false));
     }
@@ -54,18 +54,18 @@ class Home extends Component {
         && selectedPlace.id !== this.props.selectedPlace.id) {
 
       if (this.props.level === 2) {
-        this.refs.map.animateToCoordinate(selectedPlace);
+        this._map.animateToCoordinate(selectedPlace);
       }
     }
 
     if (level && level !== this.props.level) {
-      this.refs.map.updatePadding({
+      this._map.updatePadding({
         top: sizes.toolbarHeight,
         bottom: level === 1 ? - carousel.level1 : - carousel.level1 - carousel.level2
       });
 
       if (level < 3 && this.props.selectedPlace) {
-        this.refs.map.animateToCoordinate(this.props.selectedPlace);
+        this._map.animateToCoordinate(this.props.selectedPlace);
       }
     }
   }
@@ -74,7 +74,7 @@ class Home extends Component {
     this.props.dispatch(selectedPlace(place));
 
     if (this.props.level === 2) {
-      this.refs.map.animateToCoordinate(place);
+      this._map.animateToCoordinate(place);
     }
   }
 
@@ -113,18 +113,14 @@ class Home extends Component {
     this.onReviewPress(newPlaces.length && newPlaces[0] || {});
   }
 
-  onRef = (ref) => {
-    this._map = ref;
-  }
-
   onMapReady = () => {
     if (this._map && this.props.newPlace) {
-      this.refs.map.animateToCoordinate(this.props.newPlace);
+      this._map.animateToCoordinate(this.props.newPlace);
     }
   }
 
   onMapLongPress = ({coordinate}) => {
-    this.refs.searchWrapper.getWrappedInstance().searchCoordinates(coordinate);
+    this._searchWrapper.getWrappedInstance().searchCoordinates(coordinate);
     this.props.dispatch(selectNewPlace(coordinate));
   }
 
@@ -139,7 +135,7 @@ class Home extends Component {
   }
 
   onNewMarkerPress = () => {
-    this.refs.searchWrapper.getWrappedInstance().focusInput();
+    this._searchWrapper.getWrappedInstance().focusInput();
   }
 
   onNearbySelected = (place) => {
@@ -149,21 +145,21 @@ class Home extends Component {
 
   onNearbyPlaceSelected = (place) => {
     this.props.dispatch(googlePlace(place));
-    this.refs.searchWrapper.getWrappedInstance().blurInput();
-    this.refs.searchWrapper.getWrappedInstance().setValue(place.name);
+    this._searchWrapper.getWrappedInstance().blurInput();
+    this._searchWrapper.getWrappedInstance().setValue(place.name);
   }
 
   onPlaceSelected = (place) => {
     this.props.dispatch(searchedPlaces([place]));
-    this.refs.searchWrapper.getWrappedInstance().blurInput();
-    this.refs.map.animateToCoordinate(place);
+    this._searchWrapper.getWrappedInstance().blurInput();
+    this._map.animateToCoordinate(place);
   }
 
   onPlacesSelected = (place, places) => {
     this.props.dispatch(searchedPlaces(_.compact([place, ...places])));
-    this.refs.searchWrapper.getWrappedInstance().blurInput();
+    this._searchWrapper.getWrappedInstance().blurInput();
     if (place) {
-      this.refs.map.animateToCoordinate(place);
+      this._map.animateToCoordinate(place);
     }
   }
 
@@ -175,8 +171,8 @@ class Home extends Component {
     if (user.type === 'other') {
       this.props.navigation.navigate('AddFriend', { user });
     } else {
-      this.refs.searchWrapper.getWrappedInstance().blurInput();
-      this.refs.searchWrapper.getWrappedInstance().setValue(user.first_name);
+      this._searchWrapper.getWrappedInstance().blurInput();
+      this._searchWrapper.getWrappedInstance().setValue(user.first_name);
     }
   }
 
@@ -194,6 +190,7 @@ class Home extends Component {
     return (
       <SearchWrapper
         ref='searchWrapper'
+        ref={(sw) => { this._searchWrapper = sw; }}
         onClear={() => this.onSearchClear()}
         onNearbySelected={this.onNearbySelected}
         onNearbyPlaceSelected={this.onNearbyPlaceSelected}
@@ -202,7 +199,7 @@ class Home extends Component {
         onFriendPress={this.onFriendPress}
         onMenuPress={() => navigation.navigate('DrawerOpen')}>
         <Map
-          ref='map'
+          ref={(m) => { this._map = m; }}
           moveOnMarkerPress={false}
           onMapReady={this.onMapReady}
           onLongPress={this.onMapLongPress}
