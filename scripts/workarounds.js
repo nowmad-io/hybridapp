8a48b7bde470637f8c2fda9f7d283385891b1ab1
julienr2 <julien.rougeron@nouma.fr>
2018-02-21 16:37:43 +0700

    add workaround for react vector icon error

--- scripts/workarounds.js
+++ scripts/workarounds.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
 const root = path.resolve(__dirname, "..");
 
 const reactNativeCodePush = path.join(root, './node_modules/react-native-code-push/android/app/build.gradle');
 const reactNativeConfig = path.join(root, './node_modules/react-native-config/android/build.gradle');
 const buildVersion = '25.0.0';
 
+// Update Build Tool of projects with a too low version
 function updateBuildTool(path) {
   fs.readFile(path, 'utf8', function(err, contents) {
     newContents = contents.replace(/(buildToolsVersion ")(.*)(")/g, `$1${buildVersion}$3`);
@@ -16,3 +18,7 @@ function updateBuildTool(path) {
 
 updateBuildTool(reactNativeCodePush);
 updateBuildTool(reactNativeConfig);
+
+// workaround for the error: "However the module `MaterialIcons` could not be found within the package"
+const reactNativeFixtures = path.join(root, './node_modules/react-native/local-cli/core/__fixtures__/files/package.json');
+rimraf(reactNativeFixtures, {}, (err) => console.log('Error while deleting', err));
