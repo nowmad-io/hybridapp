5b686ca3f7cbc900399ff15ad7fefdf352c48a92
julienr2 <julien.rougeron@nouma.fr>
2018-03-01 12:05:44 +0700

    update package and remove codepush

--- ios/Nowmad/AppDelegate.m
+++ ios/Nowmad/AppDelegate.m
@@ -8,7 +8,6 @@
  */
 
 #import "AppDelegate.h"
-#import <CodePush/CodePush.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -19,11 +18,11 @@
 {
   NSURL *jsCodeLocation;
 
-  
+
 #ifdef DEBUG
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
-    jsCodeLocation = [CodePush bundleURL];
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
