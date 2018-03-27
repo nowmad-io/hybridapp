f2ee7ada720b110bbb27c8282397dfb811a6ba40
julienr2 <julien.rougeron@nouma.fr>
2018-03-27 11:39:48 +0700

    set up sentry !

--- android/app/src/main/java/com/nowmad/MainApplication.java
+++ android/app/src/main/java/com/nowmad/MainApplication.java
@@ -3,6 +3,7 @@ package com.nowmad;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNSentryPackage(MainApplication.this),
           new VectorIconsPackage(),
           new MapsPackage(),
           new ImagePickerPackage(),
