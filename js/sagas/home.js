da8ba9be74df0b12890b44e6811def840a581b96
julienr2 <julien.rougeron@nouma.fr>
2018-03-30 14:27:24 +0700

    group sagas into home

--- js/sagas/home.js
+++ js/sagas/home.js
@@ -1,10 +1,17 @@
 import { put, takeLatest, call, take } from 'redux-saga/effects';
+import { NavigationActions } from 'react-navigation';
 import { eventChannel } from 'redux-saga';
 
-import { RUN_SAGAS } from '../constants/utils';
+import {
+  ADD_REVIEW,
+  UPDATE_REVIEW,
+} from '../constants/reviews';
 import { GET_GEOLOCATION } from '../constants/home';
+import { RUN_SAGAS } from '../constants/utils';
 
 import { apiMe } from '../api/auth';
+import { fetchPlaces } from '../api/reviews';
+import { fetchFriends, fetchIncomingRequests, fetchOutgoingRequests } from '../api/friends';
 
 import { setGeolocation } from '../actions/home';
 
@@ -29,10 +36,23 @@ export function* currentPosition() {
 }
 
 export function* homeFlow() {
+  yield put(fetchPlaces());
   yield put(apiMe());
+  yield put(fetchFriends());
+  yield put(fetchIncomingRequests());
+  yield put(fetchOutgoingRequests());
+}
+
+function* reviewFlow(action) {
+  const { error } = action;
+
+  if (!error) {
+    yield put(NavigationActions.back());
+  }
 }
 
 export default function* root() {
   yield takeLatest(RUN_SAGAS, homeFlow);
   yield takeLatest(GET_GEOLOCATION, currentPosition);
+  yield takeLatest([`${ADD_REVIEW}_SUCCESS`, `${UPDATE_REVIEW}_SUCCESS`], reviewFlow);
 }
--- js/sagas/index.js
+++ js/sagas/index.js
@@ -1,11 +1,7 @@
 import auth from './auth';
-import friends from './friends';
-import reviews from './reviews';
 import home from './home';
 
 export default [
   auth,
   home,
-  reviews,
-  friends,
 ];
