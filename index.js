77a67cacb20ccbe3c249765dc3208e38eea10e68
julienr2 <julien.rougeron@nouma.fr>
2018-05-21 19:16:23 +0700

    refresh app with recent creat-react-native-app

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ if (Config.NODE_ENV !== 'development') {
   Sentry.config('https://800cd8afc59b49259e319e7e209e1768:ceefd4f8ccf348e49ee927c1311bcf67@sentry.io/662082').install();
 }
 
-AppRegistry.registerComponent('Nowmad', () => App);
+AppRegistry.registerComponent('nowmad', () => App);
