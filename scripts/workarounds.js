fb2a71aacb1273e273fcfb6b7e3ee7d198588779
julienr2 <julien.rougeron@nouma.fr>
2018-07-27 19:34:09 +0200

    use updated fork of react-native-maps - add workaround because of https://github.com/react-community/react-native-maps/issues/2188

--- scripts/workarounds.js
+++ scripts/workarounds.js
@@ -2,34 +2,14 @@
 
 const fs = require('fs');
 const path = require('path');
-/* eslint-disable-next-line import/no-extraneous-dependencies */
-const rimraf = require('rimraf');
 
 const root = path.resolve(__dirname, '..');
 
-const reactNativeConfig = path.join(root, './node_modules/react-native-config/android/build.gradle');
-const reactNativeFetchBlob = path.join(root, './node_modules/react-native-fetch-blob/android/build.gradle');
-const reactNativeSentry = path.join(root, './node_modules/react-native-sentry/android/build.gradle');
-const buildVersion = '25.0.0';
+const reactNativeMaps = path.join(root, './node_modules/react-native-maps/lib/android/build.gradle');
 
-// Update Build Tool of projects with a too low version
-function updateBuildTool(p) {
-  fs.readFile(p, 'utf8', (err, contents) => {
-    const newContents = contents.replace(/(buildToolsVersion ["|'])(.*)(["|'])/g, `$1${buildVersion}$3`);
+fs.readFile(reactNativeMaps, 'utf8', (err, contents) => {
+  let newContents = contents.replace(/compileOnly/g, 'provided');
+  newContents = newContents.replace(/implementation/g, 'compile');
 
-    fs.writeFileSync(p, newContents);
-  });
-}
-
-updateBuildTool(reactNativeConfig);
-updateBuildTool(reactNativeFetchBlob);
-updateBuildTool(reactNativeSentry);
-
-// workaround for the error: "However the module `MaterialIcons` could not be
-// found within the package"
-const reactNativeFixtures = path.join(root, './node_modules/react-native/local-cli/core/__fixtures__/files/package.json');
-rimraf(reactNativeFixtures, {}, (err) => {
-  if (err) {
-    console.log('Error while deleting', err);
-  }
+  fs.writeFileSync(reactNativeMaps, newContents);
 });
