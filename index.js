257e5ca986f7dc87a473e05971188c74f34b9fda
julienr2 <julien.rougeron@gmail.com>
2018-08-23 13:25:52 +0200

    remove why-did-you-update

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ if (Config.NODE_ENV !== 'development') {
   Sentry.config('https://800cd8afc59b49259e319e7e209e1768:ceefd4f8ccf348e49ee927c1311bcf67@sentry.io/662082').install();
 }
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line global-require
-  const { whyDidYouUpdate } = require('why-did-you-update');
-  whyDidYouUpdate(React);
-}
-
 const onRestartPress = () => persistor.purge().then(() => RNRestart.Restart());
 
 // Set global error handler
