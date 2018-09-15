286120d524f6716694944b32ca863abd569e1014
julienr2 <julien.rougeron@gmail.com>
2018-09-14 17:42:20 -0700

    introduces Modal component and list of predefined modals

---
+++ js/modals.js
@@ -0,0 +1,31 @@
+export const registerNoNetwork = {
+  title: 'Unable to register',
+  information: 'Sorry, unable to Register. Please check your internet connection.',
+  primaryAction: 'Ok',
+};
+
+export const registerFailed = {
+  title: 'Creation failed',
+  information: 'Sorry, an account already exists with this email. Choose an other email or try to Log-in.',
+  primaryAction: 'Ok',
+  secondaryAction: 'Log-in',
+};
+
+export const loginNoNetwork = {
+  title: 'Unable to Log-in',
+  information: 'Sorry, unable to Log-in. Please check your internet connection.',
+  primaryAction: 'Ok',
+};
+
+export const LoginFailed = {
+  title: 'Log-in failed',
+  information: 'Sorry, the email address and/or the password are incorrect. Please enter the right details or create a new account.',
+  primaryAction: 'Ok',
+  secondaryAction: 'Create an account',
+};
+
+export const profileNoNetwork = {
+  title: 'Unable to update profile',
+  information: 'Sorry, unable to update your profile. Please check your internet connection.',
+  primaryAction: 'Ok',
+};
