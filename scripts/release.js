9b5e39121c07e1ab88cf4bb95805d909f78f5b10
julienr2 <julien.rougeron@gmail.com>
2018-09-17 03:11:21 -0700

    update release script with new auth file

--- scripts/release.js
+++ scripts/release.js
@@ -11,7 +11,7 @@ const packageJson = path.join(root, './package.json');
 const packageLockJson = path.join(root, './package-lock.json');
 const androidManifest = path.join(root, './android/app/src/main/AndroidManifest.xml');
 const buildGradle = path.join(root, './android/app/build.gradle');
-const loginComponent = path.join(root, './js/components/pages/auth/login.js');
+const loginComponent = path.join(root, './js/components/pages/auth/index.js');
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 const simpleGit = require('simple-git')(root);
 
